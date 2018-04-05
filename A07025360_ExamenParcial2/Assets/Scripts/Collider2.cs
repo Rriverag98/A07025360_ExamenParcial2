19457d80a3fb14906fa58974179922df70f7d88d
Rriverag98 <rriverag98@gmail.com>
2018-04-04 22:07:46 -0500

    .

---
+++ A07025360_ExamenParcial2/Assets/Scripts/Collider2.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collider2 : MonoBehaviour { 
+    public string level;
+
+
+
+    // Method is triggered when object touches the Losecollider 
+    //Then loads a screen
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            LevelManager levelMngr = new LevelManager();
+            levelMngr.LoadLevel(level);
+        }
+    }
+}
